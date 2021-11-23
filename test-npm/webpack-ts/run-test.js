0f4689a137d145976ddd47d19279973f653d6da2
Andre Wachsmuth <sensenmann5@gmail.com>
2021-11-23 22:29:19 +0100

    use npm.cmd as a fallback (for Windows)

--- test-npm/webpack-ts/run-test.js
+++ test-npm/webpack-ts/run-test.js
@@ -1,15 +1,15 @@
 // @ts-check
 
 const path = require("path");
-const { loadHtml, spawnSimple, removeDir } = require("../helper");
+const { loadHtml, removeDir, spawnNpm } = require("../helper");
 
 module.exports.main = async () => {
   
   await removeDir(path.join(__dirname, "dist"));
   await removeDir(path.join(__dirname, "node_modules"));
   
-  await spawnSimple("npm", ["install"], __dirname);
-  await spawnSimple("npm", ["run", "build"], __dirname);
+  await spawnNpm(["install"], __dirname);
+  await spawnNpm(["run", "build"], __dirname);
   
   const jsdom  = require("jsdom");
   const { expect } = require("chai");
