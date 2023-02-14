b49530bcc02640a894fc9b95e07a3d92990ad6c6
Andre Wachsmuth <sensenmann5@gmail.com>
2023-02-15 00:40:13 +0100

    Update types and expand tests (#94)

--- test-npm/ts/global/src/index.ts
+++ test-npm/ts/global/src/index.ts
@@ -1,11 +1,106 @@
 Coloris.init();
 
-Coloris({
+Coloris.coloris({
     el: "#coloris",
+    a11y: {
+        alphaSlider: "",
+        clear: "",
+        close: "",
+        format: "",
+        hueSlider: "",
+        input: "",
+        instruction: "",
+        marker: "",
+        open: "",
+        swatch: "",
+    },
+    alpha: false,
+    clearButton: true,
+    clearLabel: "",
+    closeButton: true,
+    closeLabel: "",
+    defaultColor: "",
+    focusInput: false,
+    forceAlpha: true,
+    format: "auto",
+    formatToggle: false,
+    inline: false,
+    margin: 0,
+    onChange: (color: string) => {},
+    parent: "",
+    rtl: false,
+    selectInput: false,
+    swatches: [""],
+    swatchesOnly: false,
+    theme: "default",
+    themeMode: "auto",
+    wrap: false,
 });
 
 Coloris.coloris({
     el: "#coloris",
+    format: "auto",
+});
+Coloris.coloris({
+    el: "#coloris",
+    format: "hex",
+});
+Coloris.coloris({
+    el: "#coloris",
+    format: "hsl",
+});
+Coloris.coloris({
+    el: "#coloris",
+    format: "mixed",
+});
+Coloris.coloris({
+    el: "#coloris",
+    format: "rgb",
 });
 
+Coloris.coloris({
+    el: "#coloris",
+    theme: "default",
+});
+Coloris.coloris({
+    el: "#coloris",
+    theme: "large",
+});
+Coloris.coloris({
+    el: "#coloris",
+    theme: "pill",
+});
+Coloris.coloris({
+    el: "#coloris",
+    theme: "polaroid",
+});
+
+Coloris.coloris({
+    el: "#coloris",
+    themeMode: "auto",
+});
+Coloris.coloris({
+    el: "#coloris",
+    themeMode: "dark",
+});
+Coloris.coloris({
+    el: "#coloris",
+    themeMode: "light",
+});
+
+
 Coloris.close();
+
+Coloris.close(true);
+
+Coloris.updatePosition();
+
+Coloris.setInstance(".foo", {});
+
+Coloris.setInstance(".foo", {themeMode: "dark", onChange: (color: string) => {}});
+
+Coloris.removeInstance(".foo");
+
+document.getElementById("")?.addEventListener("coloris:pick", e => {
+    const color: string = e.detail.color;
+});
